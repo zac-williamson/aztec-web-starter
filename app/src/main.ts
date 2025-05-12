8db8e730a372e936390517ddbd5d05eece78714c
saleel <saleel@saleel.xyz>
2025-05-12 18:48:42 +0100

    refactor wallet

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,26 +1,123 @@
-import { createLogger, PXE, Wallet } from '@aztec/aztec.js';
-import { PrivateCounter } from "./counter.ts";
-import { initPXE } from './aztec.ts'
-import { createAccount, deployAccount, getAccount } from './account.ts'
+import { AztecAddress, Fr, Wallet, type AccountWallet } from '@aztec/aztec.js';
+import { LocalWallet } from './local-wallet.ts'
+import { CounterContract } from '../artifacts/Counter.ts';
 
+// Aztec Node URL
 const nodeUrl = 'http://localhost:8080';
+
+// Address of the deployed Counter contract
 const deployedContract = {
   address: "0x10f78b7ca79d779669ac6446b5fea179b2be4b2747af05839f4909166f596414",
   deployer: '0x1a41081a4e0dea2a0595258faa8531ac5cbe4a736ccd44c5c0827ca8720eae29',
   salt: '0x24d65691d1c691b19826e32faa1021f4bed7163bf6be98e78af88246a16f4163'
 }
 
-const logger = createLogger('counter');
-let ecdsaWallet: Wallet | null;
-let pxe: PXE;
-let privateCounter: PrivateCounter;
+// Local variables
+let wallet: LocalWallet;
 
 // DOM Elements
 const createAccountButton = document.querySelector<HTMLButtonElement>('#create-account')!;
 const incrementCounterButton = document.querySelector<HTMLButtonElement>('#increment-counter')!;
 const accountDisplay = document.querySelector<HTMLDivElement>('#account-display')!;
 const statusMessage = document.querySelector<HTMLDivElement>('#status-message')!;
+const counterValue = document.querySelector<HTMLDivElement>('#counter-value')!;
 
+
+// On page load
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    // Initialize the PXE and the wallet
+    displayStatusMessage('Initializing local wallet...');
+    wallet = new LocalWallet(nodeUrl);
+    await wallet.initialize();
+
+    // Register Counter contract with PXE
+    displayStatusMessage('Registering Private Counter...');
+    await wallet.registerContract(
+      CounterContract.artifact,
+      AztecAddress.fromString(deployedContract.deployer),
+      Fr.fromString(deployedContract.salt),
+      [0]
+    );
+
+    // Get existing account
+    displayStatusMessage('Checking for existing account...');
+    const account = await wallet.getAccount();
+    await displayAccount(account);
+
+    // Refresh counter if account exists
+    if (account) {
+      await updateCounterValue(account);
+      displayStatusMessage('');
+    } else {
+      displayStatusMessage('Creating new account to setup your private counter...');
+    }
+  } catch (error) {
+    displayError(error instanceof Error ? error.message : 'An unknown error occurred');
+  }
+});
+
+// Create a new account
+createAccountButton.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const button = e.target as HTMLButtonElement;
+  button.disabled = true;
+  button.textContent = 'Creating account...';
+
+  try {
+    const account = await wallet.createAccount();
+    displayAccount(account);
+
+    // Refresh counter value (not really needed though, as the default value is 0)
+    await updateCounterValue(account);
+  } catch (error) {
+    displayError(error instanceof Error ? error.message : 'An unknown error occurred');
+  } finally {
+    button.disabled = false;
+    button.textContent = 'Create Account';
+  }
+});
+
+// Increment the counter
+incrementCounterButton.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const button = e.target as HTMLButtonElement;
+  button.disabled = true;
+  button.textContent = 'Incrementing counter...';
+
+  try {
+    // Prepare contract interaction
+    const account = await wallet.getAccount();
+    const counter = await CounterContract.at(AztecAddress.fromString(deployedContract.address), account!);
+    const interaction = counter.methods.increment();
+
+    // Send transaction
+    await wallet.sendTransaction(interaction);
+
+    // Update counter value
+    updateCounterValue(account!);
+  } catch (error) {
+    displayError(error instanceof Error ? error.message : 'An unknown error occurred');
+  } finally {
+    button.disabled = false;
+    button.textContent = 'Increment Counter';
+  }
+});
+
+// Update the counter value
+async function updateCounterValue(account: Wallet) {
+  // Prepare contract interaction
+  const counter = await CounterContract.at(AztecAddress.fromString(deployedContract.address), account);
+  const interaction = counter.methods.get_counter(account.getAddress());
+
+  // Simulate the transaction
+  const value = await wallet.simulateTransaction(interaction);
+  
+  // Display the counter value
+  displayCounter(value);
+}
+
+// UI functions
 
 function displayError(message: string) {
   statusMessage.textContent = message;
@@ -34,90 +131,20 @@ function displayStatusMessage(message: string) {
   statusMessage.style.display = message ? 'block' : 'none';
 }
 
-async function refreshAccount() {
-  if (!ecdsaWallet) {
+function displayAccount(account: AccountWallet | null) {
+  if (!account) {
     createAccountButton.style.display = 'block';
     incrementCounterButton.disabled = true;
     return;
   }
 
-  const address = ecdsaWallet.getAddress().toString();
+  const address = account.getAddress().toString();
   const content = `Account: ${address.slice(0, 6)}...${address.slice(-4)}`;
   accountDisplay.textContent = content;
   createAccountButton.style.display = 'none';
   incrementCounterButton.disabled = false;
 }
 
-async function refreshCounter() {
-  if (!ecdsaWallet) {
-    return;
-  }
-
-  const currentValue = await privateCounter.getCounter(ecdsaWallet);
-  const counterValue = document.querySelector<HTMLDivElement>('#counter-value')!;
-  counterValue.textContent = currentValue.toString();
+function displayCounter(value: number) {
+  counterValue.textContent = value.toString();
 }
-
-// On page load, load the PXE, contract, account, and display the counter value
-document.addEventListener('DOMContentLoaded', async () => {
-  try {
-    displayStatusMessage('Initializing PXE...');
-    pxe = await initPXE(nodeUrl, logger);
-
-    displayStatusMessage('Initializing Private Counter...');
-    privateCounter = new PrivateCounter(
-      pxe,
-      deployedContract.address,
-      deployedContract.deployer,
-      deployedContract.salt
-    );
-    await privateCounter.initialize();
-
-    displayStatusMessage('Checking for existing account...');
-    ecdsaWallet = await getAccount(pxe);
-
-    // Deploy the account if it's not publicly deployed
-    await refreshAccount();
-    await refreshCounter();
-    
-    displayStatusMessage('');
-  } catch (error) {
-    displayError(error instanceof Error ? error.message : 'An unknown error occurred');
-  }
-});
-
-// Create a new account
-createAccountButton.addEventListener('click', async (e) => {
-  e.preventDefault();
-  (e.target as HTMLButtonElement).disabled = true;
-  (e.target as HTMLButtonElement).textContent = 'Creating account...';
-
-  try {
-    ecdsaWallet = await createAccount(pxe, logger);
-
-    await refreshAccount();
-    await refreshCounter();
-  } catch (error) {
-    displayError(error instanceof Error ? error.message : 'An unknown error occurred');
-  } finally {
-    (e.target as HTMLButtonElement).disabled = false;
-    (e.target as HTMLButtonElement).textContent = 'Create Account';
-  }
-});
-
-// Increment the counter
-incrementCounterButton.addEventListener('click', async (e) => {
-  e.preventDefault();
-  (e.target as HTMLButtonElement).disabled = true;
-  (e.target as HTMLButtonElement).textContent = 'Incrementing counter...';
-
-  try {
-    await privateCounter.incrementCounter(ecdsaWallet);
-    await refreshCounter();
-  } catch (error) {
-    displayError(error instanceof Error ? error.message : 'An unknown error occurred');
-  } finally {
-    (e.target as HTMLButtonElement).disabled = false;
-    (e.target as HTMLButtonElement).textContent = 'Increment Counter';
-  }
-});
