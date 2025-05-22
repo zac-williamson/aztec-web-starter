c11db77e476a983a251499cf609e176291ffebcc
saleel <saleel@saleel.xyz>
2025-05-22 14:03:33 +0400

    chore: update playwright config

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -7,8 +7,7 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : 3,
+  workers: process.env.WORKERS ? parseInt(process.env.WORKERS) : 9,
   reporter: 'html',
   use: {
     baseURL: 'http://127.0.0.1:3000',
